f59150429d99b3e51d3c3e69cad0c0de7d430635
Bor Dekker <32562@ma-web.nl>
2021-11-29 14:07:10 +0100

    Werkende koelkast

--- web/Les2-boodschappen/script.js
+++ web/Les2-boodschappen/script.js
@@ -1,1 +1,22 @@
 console.log("Script is geladen!");
+
+const Groentevak = document.getElementById('Groente');
+const Fruitvak = document.getElementById('Fruit');
+
+function ZetInFruitLa(Fruit){
+    console.log(Fruit)
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = Fruit;
+    nieuwElement.className = 'rood';
+    Fruitvak.appendChild(nieuwElement);
+}
+
+function ZetInGroenteLa(Groente){
+    console.log(Groente)
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = Groente;
+    nieuwElement.className = 'groen';
+    Groentevak.appendChild(nieuwElement);
+}
